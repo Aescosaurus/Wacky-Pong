418483a29762b8a4a3a621baebe3d3559fc15d2c
RadAesc <michaelbuslik@hotmail.com>
2018-10-29 13:28:15 -0600

    Added MenuController to help with interfacing with menus.

---
+++ Assets/Scripts/Menus/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController
+    :
+    MonoBehaviour
+{
+    static MenuManager menuMan;
+    // 
+    public static void Initialize()
+    {
+        menuMan = new MenuManager();
+    }
+    public static void GoToMenu( MenuState theMenu )
+    {}
+}
